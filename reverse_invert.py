b3502c688ab95033467a842d91f5d4c157d1b83e
cover711 <42560620+cover711@users.noreply.github.com>
2018-08-21 15:01:25 +0800

    Update reverse_invert.py

--- reverse_invert.py
+++ reverse_invert.py
@@ -6,3 +6,25 @@ class Solution(object):
             B.append(i[::-1])
         #print B
         return B
+'''class Solution(object):
+    def flipAndInvertImage(self, A):
+        B=[]
+        C=[]
+        D=[]
+        f=[0,0,0]
+        temp=[]
+        #a=0
+        for i in A:
+            f=[0,0,0]
+            a=0
+            for b in i:
+                if b == 1:
+                    f[a] = 0
+                else:
+                    f[a] = 1
+                a=a+1
+            D.append(f)
+            a=0
+        C=D.reverse()
+        return D
+ '''
